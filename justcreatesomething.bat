d9287a5f95df859b121b2e89dc05da8bc53dd23c
quizy123 <106913866+quizy123@users.noreply.github.com>
2022-06-21 19:22:15 +0200

    Create justcreatesomething.bat

---
+++ justcreatesomething.bat
@@ -0,0 +1,27 @@
+@echo off
+title Quizy Multi tool 
+pause
+echo welcome to quizy multi tool.
+color 3
+pause
+set /p main=Enter username:
+echo.
+set /p pass=Enter Pass:
+if %user% == user if %pass% == user goto main
+pause
+echo password succes
+echo welcome to main tool
+echo 1 - putty 2 - google
+echo.
+echo 3 - pinger
+echo.
+:mainlogo
+set /p main=Choose your number:
+if %main% == 1 goto putty.exe
+if %main% == 2 goto https://www.youtube.com/
+if %main% == 3 goto https://discord.gg/s8KDxE284D
+goto mainlogo
+:pinger
+start pinger.bat
+goto mainlogo
+pause 
